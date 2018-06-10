0111785942500964c1abc76ef6ea8385628a4901
kevinbolton729 <kbolton@sohu.com>
2018-06-10 16:42:34 +0800

    edit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import { Divider, Pagination, Button } from 'antd';
 // 样式
 import './index.less';
 // Logo
 import logo from '../../static/images/logo.svg';
 
-class Home extends Component {
+class Home extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      test: '点击我吧',
+    };
+  }
+
+  componentDidMount() {
+    console.log(this.props, 'props');
+  }
+
   render() {
+    const { test } = this.state;
     return (
       <div className="home">
         <div className="home-logo">
           <img src={logo} width="160" height="160" alt="" />
         </div>
-        <Button type="primary">Button</Button>
+        <Button type="primary">{test}</Button>
         <Divider />
         <Pagination defaultCurrent={1} total={50} showSizeChanger />
       </div>
--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-// import { Router, Route, browserHistory } from 'react-router';
+import React, { PureComponent } from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import { LocaleProvider } from 'antd';
 import zhCN from 'antd/lib/locale-provider/zh_CN';
 // 组件:Pages
@@ -7,11 +7,16 @@ import Home from './pages/Home';
 // 样式
 import './static/css/index.less';
 
-class RouterConfig extends Component {
+class RouterConfig extends PureComponent {
   render() {
     return (
       <LocaleProvider locale={zhCN}>
-        <Home />
+        <Router>
+          <Switch>
+            <Route exact path="/home" component={Home} />
+            <Redirect from="/" to="/home" />
+          </Switch>
+        </Router>
       </LocaleProvider>
     );
   }
